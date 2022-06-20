62ad788efcb30a38fe6db3ea8a93b3e845b1f8b6
Francisco <fsousa.jpa@gmail.com>
2022-06-20 20:01:12 -0300

    criando o model, repository e resource

---
+++ src/main/java/com/esdraz/algamoney/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.esdraz.algamoney.repository;
+
+import com.esdraz.algamoney.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+
+}
